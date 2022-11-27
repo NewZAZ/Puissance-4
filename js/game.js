8bfec22349f49fbcfd2735ca706e3f87a7dd4089
ben.tintin.bp@gmail.com <ben.tintin.bp@gmail.com>
2022-11-27 16:01:08 +0100

    work on click event !

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const board = []
 
+let currentPlayer = 1;
+
 function initBoard(size) {
 
 
@@ -25,14 +27,15 @@ function showBoard() {
     for (let i = 0; i < board.length; i++) {
         insertion += "<tr>";
         for (let j = 0; j < board[i].length; j++) {
-            insertion += "<td id="+i+','+j+">";
+            insertion += "<td id="+j+">";
             switch (board[i][j]){
                 case 1 :
-                    insertion += "<img width='52'height='52' src='img/blue.png'>";
+                    insertion += "<img id="+j+" width='52'height='52' src='img/blue.png'>";
                     break;
                 case 2 :
-                    insertion += "<img width='52'height='52' src='img/red.png'>";
+                    insertion += "<img id="+j+" width='52'height='52' src='img/red.png'>";
                     break;
+                default:insertion += "<img id="+j+" width='52'height='52' src='img/blank.png'>";
             }
             insertion += "</td>";
 
@@ -48,28 +51,34 @@ function showBoard() {
 initBoard(10)
 
 function checkWin() {
+    if (checkColumn()) {
+        console.log("GG 1")
+    }
 
+    if(checkRow()){
+        console.log("GG 2")
+    }
 }
 
 function checkColumn() {
-
+    player = currentPlayer
     for (let y = 0; y < board.length; y++) {
         countSameColor = 0
-        player = 0
+
         for (let x = 0; x < board.length; x++) {
-            if (player == board[x][y]) {
+            if(countSameColor  >= 4){
+                return true;
+            }
+            if (player == board[x][y] ) {
                 countSameColor++;
             } else if (countSameColor > 0) {
                 countSameColor = 0
-                player = board[x][y]
             }
         }
 
-        if (countSameColor >= 4) {
-            return true;
-        }
 
     }
+    return false;
 }
 
 function checkDiagonal() {
@@ -102,18 +111,22 @@ function checkDiagonal() {
     showBoard();
 }
 
-checkDiagonal();
+showBoard()
+
+//checkDiagonal();
 
 function checkRow() {
+    player = currentPlayer
     for (let x = 0; x < board.length; x++) {
         countSameColor = 0
-        player = 0
         for (let y = 0; y < board.length; y++) {
-            if (player == board[x][y]) {
+            if(countSameColor  >= 4){
+                return true;
+            }
+            if (player == board[x][y] ) {
                 countSameColor++;
             } else if (countSameColor > 0) {
                 countSameColor = 0
-                player = board[x][y]
             }
         }
 
@@ -121,5 +134,49 @@ function checkRow() {
             return true;
         }
 
+    }
+}
+
+addEventListener("click", ev => {
+    const ID = ev.target.id;
+
+        const Y = parseInt(ID);
+
+        if(!columnIsFull(Y)){
+            board[getLastXByColumn(Y) -1][Y] = currentPlayer;
+            checkWin()
+            switchPlayer();
+            showBoard()
+        }else {
+            console.log("nop")
+        }
+});
+
+function columnIsFull(y){
+    for(let x = 0; x < board.length; x++){
+        if (board[x][y] == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function getLastXByColumn(y){
+    for(let x = 0; x < board.length; x++){
+        if(board[x][y] != 0){
+            return x;
+        }
+    }
+    return board.length
+}
+
+function switchPlayer(){
+    switch (currentPlayer){
+        case 1:
+            currentPlayer = 2
+            break;
+        case 2:
+            currentPlayer = 1
+            break;
     }
 }
