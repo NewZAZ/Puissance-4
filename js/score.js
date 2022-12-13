3c1ed0a56aa16dda9741e12d66b0659bb5046ada
ben.tintin.bp@gmail.com <ben.tintin.bp@gmail.com>
2022-12-13 20:23:10 +0100

    update !

--- js/score.js
+++ js/score.js
@@ -1,8 +1,8 @@
 function displayScore(){
-    if(lastScore.length =0) return
+    if(lastScore.length === 0) return
     for(let i = 0; i < lastScore.length; i++){
         const lastScoreElement = lastScore[i];
-        const elementById = document.getElementById(str(lastScoreElement));
+        const elementById = document.getElementById(lastScoreElement.toString());
 
         elementById.innerText = lastScoreElement[0] +" VS "+ lastScoreElement[1]
     }
